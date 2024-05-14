b4cd1a100879bad1a36f03470dff0e83701ce684
myac-2022020@kinal.edu.gt <myac-2022020@kinal.edu.gt>
2024-05-13 21:45:10 -0600

    Main hotel management backend structure

---
+++ HotelManagementReact-Kinalitos/src/pages/Room/Room.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Room = () => {
+  return (
+    <div>room</div>
+  )
+}
---
+++ HotelManagementReact-Kinalitos/src/routes.jsx
@@ -0,0 +1,8 @@
+import { Room } from "./pages/Room/Room.jsx"
+
+export const routes = [
+    {
+        path: '/room',
+        element: <Room/>
+    }
+]
